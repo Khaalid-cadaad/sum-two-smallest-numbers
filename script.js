4acac1d8c9f50cfdffeb3e2f3479481f13ea5dd8
unknown <khalidabdi3101@gmail.com>
2024-10-05 05:18:12 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Good Luck 💪🏾
+function sumTwoSmallestNums(arr) {
+  
+    const sortedArr = arr.sort((a, b) => a - b);
+    
+    
+    return sortedArr[0] + sortedArr[1];
+}
+
+
+console.log(sumTwoSmallestNums([12, 5, 24, 55, 18])); 
+console.log(sumTwoSmallestNums([190, 300, 230, 800]));
